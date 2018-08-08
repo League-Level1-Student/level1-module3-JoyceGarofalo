6cf102c48b692d55b91c847e20e6b1e013fe6055
league <league@wts22.attlocal.net>
2018-08-08 12:01:24 -0700

    calculator, get latest tweet, pig latin translator

--- src/Calculator.java
+++ src/Calculator.java
@@ -7,23 +7,27 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class Calculator implements ActionListener{
+public class Calculator implements ActionListener {
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JButton add = new JButton();
 	JButton sub = new JButton();
 	JButton mul = new JButton();
 	JButton div = new JButton();
-	JLabel label = new JLabel();
-	JTextField left = new JTextField();
-	JTextField right = new JTextField();
+	JLabel label = new JLabel("answer");
+	JTextField left = new JTextField("num1");
+	JTextField right = new JTextField("num2");
+
 	public static void main(String[] args) {
 		Calculator calc = new Calculator();
 		calc.CreateUI();
 	}
+
 	public void CreateUI() {
-		frame.setSize(200, 200);
+		frame.setSize(400, 400);
 		frame.add(panel);
+		panel.add(left);
+		panel.add(right);
 		panel.add(add);
 		panel.add(sub);
 		panel.add(mul);
@@ -34,15 +38,51 @@ public class Calculator implements ActionListener{
 		sub.addActionListener(this);
 		mul.addActionListener(this);
 		div.addActionListener(this);
+		add.setText("add");
+		sub.setText("sub");
+		mul.setText("mul");
+		div.setText("div");
+		frame.pack();
 	}
-	
+
 	public void add() {
-		label = (left.getText())+(right.getText());
+		double l = Integer.parseInt(left.getText());
+		double r = Integer.parseInt(right.getText());
+		label.setText("" + (l + r));
 	}
-	
+
+	public void sub() {
+		double l = Integer.parseInt(left.getText());
+		double r = Integer.parseInt(right.getText());
+		label.setText("" + (l - r));
+	}
+
+	public void mul() {
+		double l = Integer.parseInt(left.getText());
+		double r = Integer.parseInt(right.getText());
+		label.setText("" + (l * r));
+	}
+
+	public void div() {
+		double l = Integer.parseInt(left.getText());
+		double r = Integer.parseInt(right.getText());
+		label.setText("" + (l / r));
+	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
+		if (e.getSource().equals(add)) {
+			add();
+		}
+		if (e.getSource().equals(sub)) {
+			sub();
+		}
+		if (e.getSource().equals(mul)) {
+			mul();
+		}
+		if (e.getSource().equals(div)) {
+			div();
+		}
 	}
 }
